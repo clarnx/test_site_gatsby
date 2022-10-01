059c6834bac851b08e334ceea182aae0e84f616b
A.E Clarence <33183127+clarnx@users.noreply.github.com>
2022-10-01 12:53:46 +0000

    Update imprint.js

--- src/pages/imprint.js
+++ src/pages/imprint.js
@@ -4,8 +4,9 @@ import { LegalSection, Page, Seo } from "gatsby-theme-portfolio-minimal";
 export default function ImprintPage() {
   return (
     <>
-      <Seo title="Imprint" useTitleTemplate={true} noIndex={true} />
+      
       <Page>
+        <Seo title="Imprint" useTitleTemplate={true} noIndex={true} />
         <LegalSection sectionId="imprint" heading="Imprint" />
       </Page>
     </>
